20f44aa0fe82dfde80b48f103fa3b0a59558f0f1
Parker Timmerman <parker@parkertimmerman.com>
2022-05-27 19:51:27 -0400

    branch start, upgrade to compact_str v0.4

--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -1,10 +1,10 @@
-use compact_str::CompactStr;
+use compact_str::CompactString;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct NetworkPacket {
     id: i64,
-    r#type: CompactStr,
+    r#type: CompactString,
     body: Vec<u8>,
     payload_transfer_info: Vec<u8>,
     payload_size: i64,
